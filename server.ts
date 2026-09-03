0b1dd7ee9253c83a84451149433af8c4952fe1ad
Brayan Perez <brayanperezdev@gmail.com>
2026-09-03 04:25:41 +0000

    feat(server): integrate dotenv for environment variable management and update API versioning

--- server.ts
+++ server.ts
@@ -1,13 +1,22 @@
 import express, { Application } from "express";
+import dotenv from "dotenv";
 import auctionsRouter from "./routes/auctions_routes";
+import authRouter from "./routes/auth_routes";
+import usersRouter from "./routes/users_routes";
+import paymentsRouter from "./routes/payments_routes";
 
-const PORT: number = 3000;
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT) || 3000;
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.use("/api", auctionsRouter);
+app.use("/api/v1", auctionsRouter);
+app.use("/api/v1", authRouter);
+app.use("/api/v1", usersRouter);
+app.use("/api/v1", paymentsRouter);
 
 app.listen(PORT, () => {
     console.log(`SERVER RUNNING ON PORT ${PORT}!`);
